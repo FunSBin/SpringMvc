2ab143add7387186cf428cba88a4f4221ca7a738
FunSBin <38375464@qq.com>
2021-08-26 15:08:04 +0800

    自定义类型格式 提交一部分

---
+++ src/main/java/com/one/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.one.controller;
+
+import com.one.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+import java.util.SortedMap;
+
+@Controller
+public class UserController {
+
+
+    @RequestMapping(value = "/setUser/{name}/{age}/{birthDate}")
+    public String setUser(
+//            User user
+            @PathVariable("name") String name,
+            @PathVariable("age") Integer age,
+            @PathVariable("birthDate") String birthDate
+
+    ){
+//        System.out.println(user);
+        System.out.println(name+age+"-"+birthDate);
+        return "test";
+    }
+
+}
---
+++ src/main/java/com/one/model/User.java
@@ -0,0 +1,43 @@
+package com.one.model;
+
+import java.util.Date;
+
+public class User {
+    private String name;
+    private Integer age;
+    private Date birthDate;
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+}
