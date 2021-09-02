1abba731a4bf4254cfe9125efd1fe235a3b2edf3
FunSBin <38375464@qq.com>
2021-09-02 17:15:20 +0800

    20210902 下班提交一版，

--- src/main/java/com/one/util/MyConverterUtil.java
+++ src/main/java/com/one/util/MyConverterUtil.java
@@ -5,16 +5,16 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 
-@Component
-public class MyConverterUtil implements Converter<String, User> {
-    public User convert(String source) {
-        User user = null;
-        String[] split = source.split("-");
-        if (source!=null && split.length==4){
-            user = new User();
-            user.setName(split[1]);
-            user.setAge(Integer.parseInt(split[2]));
-        }
-        return user;
-    }
-}
+//@Component
+//public class MyConverterUtil implements Converter<String, User> {
+////    public User convert(String source) {
+////        User user = null;
+////        String[] split = source.split("-");
+////        if (source!=null && split.length==4){
+////            user = new User();
+////            user.setName(split[1]);
+////            user.setAge(Integer.parseInt(split[2]));
+////        }
+////        return user;
+////    }
+//}
